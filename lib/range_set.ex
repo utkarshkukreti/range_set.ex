bb7de568dbd8507a46b498e3c655e373f2878199
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2016-09-13 15:31:07 +0530

    feat(RangeSet.{put/3,contains?/2})

--- lib/range_set.ex
+++ lib/range_set.ex
@@ -4,4 +4,49 @@ defmodule RangeSet do
   def new do
     %RangeSet{root: nil}
   end
+
+  def contains?(%RangeSet{root: root}, term), do: do_contains?(root, term)
+
+  defp do_contains?(nil, _), do: false
+  defp do_contains?({_, low, _, left, _}, term) when term < low do
+    do_contains?(left, term)
+  end
+  defp do_contains?({_, _, high, _, right}, term) when term > high do
+    do_contains?(right, term)
+  end
+  defp do_contains?(_, _), do: true
+
+  def put(%RangeSet{} = range_set, a, b) when a > b, do: put(range_set, b, a)
+  def put(%RangeSet{root: root}, a, b) do
+    %RangeSet{root: do_put(root, a, b)}
+  end
+
+  defp do_put(nil, a, b), do: {1, a, b, nil, nil}
+  defp do_put({_height, low, high, left, right}, a, b) when b < low do
+    left = do_put(left, a, b)
+    {1 + max(h(left), h(right)), low, high, left, right}
+  end
+  defp do_put({_height, low, high, left, right}, a, b) when a > high do
+    right = do_put(right, a, b)
+    {1 + max(h(left), h(right)), low, high, left, right}
+  end
+  defp do_put({_height, low, high, _left, _right} = node, a, b) when a >= low and b <= high do
+    node
+  end
+  defp do_put({_height, low, high, left, right}, a, b) when a < low and b <= high do
+    left = do_put(left, a, low)
+    {1 + max(h(left), h(right)), low, high, left, right}
+  end
+  defp do_put({_height, low, high, left, right}, a, b) when a >= low and b > high do
+    right = do_put(right, high, b)
+    {1 + max(h(left), h(right)), low, high, left, right}
+  end
+  defp do_put({_height, low, high, left, right}, a, b) when a < low and b > high do
+    left = do_put(left, a, low)
+    right = do_put(right, high, b)
+    {1 + max(h(left), h(right)), low, high, left, right}
+  end
+
+  defp h(nil), do: 0
+  defp h({height, _, _, _, _}), do: height
 end
--- test/range_set_test.exs
+++ test/range_set_test.exs
@@ -5,4 +5,32 @@ defmodule RangeSetTest do
   test "new/0" do
     assert %RangeSet{root: nil} = RangeSet.new
   end
+
+  test "put/3 and contains?/2" do
+    # The number of ranges to put in the RangeSet.
+    sizes = 1..32
+    # The ranges of integers to insert in the RangeSet.
+    maximums = [0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+    # The number of tests to run per size of RangeSet.
+    tests = 100
+    # The number of random integers to check for contains?/2
+    checks = 100
+
+    for size <- sizes do
+      for maximum <- maximums do
+        for _ <- 1..tests do
+          ranges = for _ <- 1..size, do: Enum.random(0..maximum)..Enum.random(0..maximum)
+          range_set = Enum.reduce(ranges, RangeSet.new, fn a..b, range_set ->
+            RangeSet.put(range_set, a, b)
+          end)
+          for integer <- Enum.take_random(1..maximum, checks) do
+            # A simple test that's hopefully correct.
+            actually_contains = Enum.any?(ranges, fn range -> integer in range end)
+            # The above should match RangeSet.contains?/2
+            assert actually_contains == RangeSet.contains?(range_set, integer)
+          end
+        end
+      end
+    end
+  end
 end
