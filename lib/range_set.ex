26dbf95ea203c69910f3181fdf1e528605aa0eee
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2016-09-18 23:21:43 +0530

    feat(RangeSet.Node): also move almost all RangeSet logic to it

--- lib/range_set.ex
+++ lib/range_set.ex
@@ -1,90 +1,18 @@
 defmodule RangeSet do
+  alias RangeSet.Node
+
   defstruct [:root]
 
   def new do
-    %RangeSet{root: nil}
+    %RangeSet{root: Node.new}
   end
 
-  def contains?(%RangeSet{root: root}, term), do: do_contains?(root, term)
-
-  defp do_contains?(nil, _), do: false
-  defp do_contains?({_, low, _, left, _}, term) when term < low do
-    do_contains?(left, term)
+  def contains?(%RangeSet{root: root}, term) do
+     Node.contains?(root, term)
   end
-  defp do_contains?({_, _, high, _, right}, term) when term > high do
-    do_contains?(right, term)
-  end
-  defp do_contains?(_, _), do: true
 
   def put(%RangeSet{} = range_set, a, b) when a > b, do: put(range_set, b, a)
   def put(%RangeSet{root: root}, a, b) do
-    %RangeSet{root: do_put(root, a, b)}
-  end
-
-  defp do_put(nil, a, b), do: {1, a, b, nil, nil}
-  defp do_put({_height, low, high, left, right}, a, b) when b < low do
-    left = do_put(left, a, b)
-    n(low, high, left, right) |> do_balance
-  end
-  defp do_put({_height, low, high, left, right}, a, b) when a > high do
-    right = do_put(right, a, b)
-    n(low, high, left, right) |> do_balance
-  end
-  defp do_put({_height, low, high, _left, _right} = node, a, b) when a >= low and b <= high do
-    node
-  end
-  defp do_put({_height, low, high, left, right}, a, b) when a < low and b <= high do
-    left = do_put(left, a, low)
-    n(low, high, left, right) |> do_balance
-  end
-  defp do_put({_height, low, high, left, right}, a, b) when a >= low and b > high do
-    right = do_put(right, high, b)
-    n(low, high, left, right) |> do_balance
-  end
-  defp do_put({_height, low, high, left, right}, a, b) when a < low and b > high do
-    left = do_put(left, a, low)
-    right = do_put(right, high, b)
-    n(low, high, left, right) |> do_balance
-  end
-
-  defp do_rotate_left({_, low, high, left, {_, rlow, rhigh, rleft, rright}}) do
-    n(rlow, rhigh, n(low, high, left, rleft), rright)
-  end
-
-  defp do_rotate_right({_, low, high, {_, llow, lhigh, lleft, lright}, right}) do
-    n(llow, lhigh, lleft, n(low, high, lright, right))
-  end
-
-  defp do_balance(nil), do: nil
-  defp do_balance({_, low, high, left, right} = node) do
-    case h(left) - h(right) do
-      2 ->
-        {lheight, llow, lhigh, lleft, lright} = left
-        if h(lleft) - h(lright) > -1 do
-          # Left Left
-          node
-        else
-          # Left Right
-          n(low, high, left |> do_rotate_left, right)
-        end |> do_rotate_right
-      -2 ->
-        {rheight, rlow, rhigh, rleft, rright} = right
-        if h(rleft) - h(rright) < 1 do
-          # Right Right
-          node
-        else
-          # Right Left
-          n(low, high, left, right |> do_rotate_right)
-        end |> do_rotate_left
-      _ ->
-        node
-    end
-  end
-
-  defp h(nil), do: 0
-  defp h({height, _, _, _, _}), do: height
-
-  defp n(low, high, left, right) do
-    {1 + max(h(left), h(right)), low, high, left, right}
+    %RangeSet{root: Node.put(root, a, b)}
   end
 end
---
+++ lib/range_set/node.ex
@@ -0,0 +1,81 @@
+defmodule RangeSet.Node do
+  def new do
+    nil
+  end
+
+  def new(low, high, left, right) do
+    {1 + max(height(left), height(right)), low, high, left, right}
+  end
+
+  def contains?(nil, _), do: false
+  def contains?({_, low, _, left, _}, term) when term < low do
+    contains?(left, term)
+  end
+  def contains?({_, _, high, _, right}, term) when term > high do
+    contains?(right, term)
+  end
+  def contains?(_, _), do: true
+
+  def put(nil, a, b), do: {1, a, b, nil, nil}
+  def put({_height, low, high, left, right}, a, b) when b < low do
+    left = put(left, a, b)
+    new(low, high, left, right) |> balance
+  end
+  def put({_height, low, high, left, right}, a, b) when a > high do
+    right = put(right, a, b)
+    new(low, high, left, right) |> balance
+  end
+  def put({_height, low, high, _left, _right} = node, a, b) when a >= low and b <= high do
+    node
+  end
+  def put({_height, low, high, left, right}, a, b) when a < low and b <= high do
+    left = put(left, a, low)
+    new(low, high, left, right) |> balance
+  end
+  def put({_height, low, high, left, right}, a, b) when a >= low and b > high do
+    right = put(right, high, b)
+    new(low, high, left, right) |> balance
+  end
+  def put({_height, low, high, left, right}, a, b) when a < low and b > high do
+    left = put(left, a, low)
+    right = put(right, high, b)
+    new(low, high, left, right) |> balance
+  end
+
+  def rotate_left({_, low, high, left, {_, rlow, rhigh, rleft, rright}}) do
+    new(rlow, rhigh, new(low, high, left, rleft), rright)
+  end
+
+  def rotate_right({_, low, high, {_, llow, lhigh, lleft, lright}, right}) do
+    new(llow, lhigh, lleft, new(low, high, lright, right))
+  end
+
+  def balance(nil), do: nil
+  def balance({_, low, high, left, right} = node) do
+    case height(left) - height(right) do
+      2 ->
+        {lheight, llow, lhigh, lleft, lright} = left
+        if height(lleft) - height(lright) > -1 do
+          # Left Left
+          node
+        else
+          # Left Right
+          new(low, high, left |> rotate_left, right)
+        end |> rotate_right
+      -2 ->
+        {rheight, rlow, rhigh, rleft, rright} = right
+        if height(rleft) - height(rright) < 1 do
+          # Right Right
+          node
+        else
+          # Right Left
+          new(low, high, left, right |> rotate_right)
+        end |> rotate_left
+      _ ->
+        node
+    end
+  end
+
+  def height(nil), do: 0
+  def height({height, _, _, _, _}), do: height
+end
